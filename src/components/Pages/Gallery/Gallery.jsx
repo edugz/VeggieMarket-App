080a8f2d5f956b2d946ad2d41c25bb7599cb2ae3
edgz98 <edgz98@hotmail.com>
2024-10-16 13:05:16 +0900

    Minor update, fixed Home Button in Gallery Page

--- src/components/Pages/Gallery/Gallery.jsx
+++ src/components/Pages/Gallery/Gallery.jsx
@@ -1,15 +1,16 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Gallery.css";
 
 function Gallery() {
   return (
     <div className="gallery-main">
       <div className="gallery">Gallery</div>
-      <button className="go-back-button">Back</button>;
+      <Link to="/" style={{ textDecoration: "none" }}>
+        <button className="go-back-button">Back</button>;
+      </Link>
     </div>
   );
 }
 
 export default Gallery;
-
-
